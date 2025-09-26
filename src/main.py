3491671706b86c13a19f002fbf214a3d1cb43fbb
Weverson Barbieri <weverson.barbieri@gmail.com>
2025-09-26 16:57:14 -0300

    commit code example

--- src/main.py
+++ src/main.py
@@ -1,16 +1,12 @@
 import streamlit as st
 from st_supabase_connection import SupabaseConnection
 
-# Init connecttion
-conn = st.connection(
-    name="supabase", 
-    type=SupabaseConnection,
-    url=st.secrets["connections"]["supabase"]["NEXT_PUBLIC_SUPABASE_URL"],
-    key=st.secrets["connections"]["supabase"]["NEXT_PUBLIC_SUPABASE_ANON_KEY"]
-)
+# Initialize connection.
+conn = st.connection("supabase",type=SupabaseConnection)
 
-rows = conn.query("*", table="terminal_commands", ttl="10m").execute()
+# Perform query.
+rows = conn.query("*", table="mytable", ttl="10m").execute()
 
+# Print results.
 for row in rows.data:
-    st.write(f"Command: {row}")
-
+    st.write(f"{row['name']} has a :{row['pet']}:")
